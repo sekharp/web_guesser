a9ec65f25b4dd35d79a0d1a99fd48ec5a71b6828
Sekhar Paladugu <sekharp@gmail.com>
2015-10-05 13:15:47 -0600

    I3 done

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,6 +2,17 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 number = rand(100)
+too_high_message = "Way too high!"
+high_message = "Too high!"
+low_message = "Too low!"
+too_low_message = "Way too low!"
+correct = "You got it right!"
+
 get '/' do
-  erb :index, :locals => {:number => number}
+  erb :index, :locals => {:number => number,
+                          :too_high_message => too_high_message,
+                          :high_message => high_message,
+                          :low_message => low_message,
+                          :too_low_message => too_low_message,
+                          :correct => correct}
 end
