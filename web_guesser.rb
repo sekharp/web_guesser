ed857551b51361a9acf7729b87e35f4f919e5cc2
Sekhar Paladugu <sekharp@gmail.com>
2015-10-06 21:55:14 -0600

    ready for submission

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,24 +4,34 @@ require 'sinatra/reloader'
 NUMBER = rand(100)
 
 def check_guess(guess)
-
-  guess = guess.to_i
-  if guess > NUMBER + 5
+  if guess.nil?
+    ""
+  elsif guess.to_i > NUMBER + 5
     "Way too high!"
-  elsif guess > NUMBER
+  elsif guess.to_i > NUMBER
     "Too high!"
-  elsif guess < NUMBER
+  elsif guess.to_i < NUMBER
     "Too low!"
-  elsif guess < NUMBER - 5
+  elsif guess.to_i < NUMBER - 5
     "Way too low!"
   else
     "You got it right! The SECRET NUMBER is #{NUMBER}"
   end
 end
 
+def cheat_message(cheat)
+  if cheat == "true"
+    "The SECRET NUMBER is #{NUMBER}"
+  end
+end
+
 get '/' do
   guess = params["guess"]
+  cheat = params["cheat"]
   message = check_guess(guess)
+  hint = cheat_message(cheat)
+
   erb :index, :locals => {:number => NUMBER,
-                          :message => message}
+                          :message => message,
+                          :hint => hint}
 end
